00e810fc5cba622ec8d19326335026a63fde0362
hyunjoo.kim <khjoo1203@gmail.com>
2025-07-18 13:46:23 +0900

    유투브 비디오 목록 만들기

---
+++ src/pages/Videos.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import VideoCard from '../components/VideoCard';
+import axios from 'axios';
+
+export default function Videos() {
+  const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery({
+    queryKey: ['videos', keyword],
+    queryFn: async () => {
+      const response = await axios.get(
+        `/videos/${keyword ? 'search' : 'popular'}.json`
+      );
+      return response.data.items;
+    },
+  });
+
+  return (
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 😖</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#FF0000'
+      },
+    },
+  },
+  plugins: [],
+};
